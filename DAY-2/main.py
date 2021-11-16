12435ed8c9661209de6f9304055de84c9bc37df2
Prashant kumar <prashantkumarpd@gmail.com>
2021-11-16 18:23:39 +0530

    DSA2

---
+++ DAY-2/main.py
@@ -0,0 +1,70 @@
+print("hyy")
+
+'''Datatype and variable'''
+
+'''
+1.numeric value
+a.int
+b.float
+c.complex
+'''
+a=5
+print(type(a))
+a=5.3
+print(type(a))
+a=5+4j
+print(type(a))
+
+'''
+2.sequence type
+a.string
+b.list
+c.tuple
+'''
+name="my name is prashant kumar"
+print(name)
+print(type(name))
+name1=['my name is prashanr kumar']
+print(name1)
+print(type(name1))
+name2=(1,2,3,4)
+print(name2)
+print(type(name2))
+
+'''
+3.Boolean
+'''
+p=True
+q=False
+print(p)
+print(type(p))
+
+'''
+4.set
+'''
+z=[1,2,3,3]
+print(type(set(z)))
+
+'''
+5.Dictionary
+'''
+f={2:4,5:5,6:7}
+print(type(f))
+
+'''Python Arithmetic Operators'''
+x = 5
+y = 3
+
+print(x + y)  #addition
+print(x - y)  #subtraction
+print(x * y)  #multipliaction
+print(x / y)  # division
+print(x % y)  # modulus
+print(x ** y)  #exponentiation
+print(x // y)  #floor division
+print(x == y)  #equal to
+print(x != y)   # not equal
+print(x > y)   # x greater than y
+print(x >= y)  # x greater than eual to y
+print(x < y)   # x less than y
+print(x <= y)  # x less than euqal to y
