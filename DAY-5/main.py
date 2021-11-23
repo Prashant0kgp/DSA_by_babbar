15f48442262eaec165e586cd87297e7ef604677f
Prashant kumar <90179696+Prashant0kgp@users.noreply.github.com>
2021-11-24 01:34:57 +0530

    Add files via upload

---
+++ DAY-5/main.py
@@ -0,0 +1,49 @@
+a=4
+b=6
+print("and ", a&b)
+print("or",a|b)
+print("not",~a)
+print('XOR',a^b)
+print(5<<3)
+a,b=1,1
+a=10
+a=+1
+if (a):
+    print(b)
+else:
+    b=+1
+    print(b)
+
+a,b=1,2
+a=-1
+# i+=1
+b=+1
+if (a>0 & b>2):
+    print("Stage-Inside if")
+else:
+    print('stage-inside else')
+print(a,' ',b)
+n=int(input('enter the value: '))
+for i in range(1,n+1):
+    print(i)
+sume=0
+for i in range(1,n+1):
+    sume+=i
+print(sume)
+a=0
+b=1
+print(a)
+print(b)
+for i in range(10):
+    nxt=a+b
+    print(nxt)
+    a=b
+    b=nxt
+for i in range(5):
+    print("wdw")
+    print("dwd")
+    continue
+    print('bfbu')
+for i in range(5,0,-1):
+    print(i)
+    # i=i+1
